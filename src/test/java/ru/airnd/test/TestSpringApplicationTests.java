075bbb9be88a704851e9919a6bdb1e76c8597ed3
Grig Alex <alexengrigdev@gmail.com>
2023-12-13 13:39:08 +0300

    Init commit

---
+++ src/test/java/ru/airnd/test/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.airnd.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
